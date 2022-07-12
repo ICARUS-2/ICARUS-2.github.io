92c202373c4b2a84980e23e5a99b3c86461de69f
ICARUS-2 <ebriffett01@gmail.com>
2022-07-12 13:55:05 -0400

    Added express and Mongodb to technologies'

--- docs/apps/mo-alt/dashboard/dash-script.js
+++ docs/apps/mo-alt/dashboard/dash-script.js
@@ -199,13 +199,8 @@ const MONERO_ADDR_LENGTH = 95;
 const MONERO_INTEGR_ADDR_LENGTH = 106;
 document.addEventListener("DOMContentLoaded", PreparePage)
 
-//Top API status
-let topApiStatusDisplay;
-let topApiStatusBlinkerDisplay;
-
 //Top status
 let topStatusDisplay;
-let statusBlinkerDisplay;
 
 //Top stat displays
 let networkHashrateDisplay;
@@ -258,7 +253,6 @@ function PreparePage()
     SetEventListeners();
     GetDisplays();
     InitializeBlockDropdownMenu();
-    InitiateBlinker();
 
     let displays = document.getElementsByClassName("display");
     for (let d of displays)
@@ -450,13 +444,8 @@ function SetEventListeners()
 
 function GetDisplays()
 {
-    //Top API status
-    topApiStatusDisplay = document.getElementsByClassName("dashboardApiStatusDisplay")[0]
-    topApiStatusBlinkerDisplay = document.getElementsByClassName("apiStatusBlinkerDisplay")[0]
-
     //Top status
     topStatusDisplay = document.getElementsByClassName("dashboardStatusDisplay")[0];
-    statusBlinkerDisplay = document.getElementsByClassName("statusBlinkerDisplay")[0];
 
     //Top stats
     networkHashrateDisplay = document.getElementsByClassName("networkHashrateDisplay")[0];
@@ -521,42 +510,29 @@ async function RefreshStats()
     let minerStatsAllWorkersUrl = minerStatsUrl + "/allWorkers"
     let poolStatsUrl = "https://api.moneroocean.stream/pool/stats";
     let networkStatsUrl = "https://api.moneroocean.stream/network/stats";
+    let worldUrl = "https://localmonero.co/blocks/api/get_stats";
+    //let xmrBlocksUrl = "https://api.moneroocean.stream/pool/blocks";
+    //let altBlocksUrl = "https://api.moneroocean.stream/pool/altblocks";
     let userUrl = baseUrl;
     userUrl += "user/" + addr;
 
-    let didApiCallSucceed = true;
-
-    let networkStatsObj;
-    let minerStatsObj;
-    let minerStatsAllWorkersObj;
-    let poolStatsObj;
-    let userObj;
-    try
-    {
-        networkStatsObj = await FetchJson(networkStatsUrl);
-        minerStatsObj = await FetchJson(minerStatsUrl);
-        minerStatsAllWorkersObj = await FetchJson(minerStatsAllWorkersUrl);
-        poolStatsObj = await FetchJson(poolStatsUrl);
-        userObj = await FetchJson(userUrl);
-    }
-    catch(err)
-    {
-        console.log("MoneroOcean API call failed.")
-        didApiCallSucceed = false;
-    }
+    let worldApiObj = await FetchJson(worldUrl);
+    let networkStatsObj = await FetchJson(networkStatsUrl);
+    let minerStatsObj = await FetchJson(minerStatsUrl);
+    let minerStatsAllWorkersObj = await FetchJson(minerStatsAllWorkersUrl);
+    let poolStatsObj = await FetchJson(poolStatsUrl);
+    //let xmrBlocksObj = await FetchJson(xmrBlocksUrl);
+    //let altBlocksObj = await FetchJson(altBlocksUrl);
+    let userObj = await FetchJson(userUrl);
 
     UpdateStatusBar(minerStatsAllWorkersObj);
-
-    if(didApiCallSucceed)
-    {
-        UpdateTopStats(networkStatsObj, poolStatsObj)
-        UpdateMinerHashrates(minerStatsObj);
-        UpdateConnectedMiners(poolStatsObj, minerStatsAllWorkersObj);
-        UpdateBalances(minerStatsObj, userObj, poolStatsObj);
-        UpdateExchangeRates(poolStatsObj);
-        UpdateMinerData(minerStatsAllWorkersObj)
-        UpdateBlockData()
-    }
+    UpdateTopStats(networkStatsObj, poolStatsObj, worldApiObj)
+    UpdateMinerHashrates(minerStatsObj);
+    UpdateConnectedMiners(poolStatsObj, minerStatsAllWorkersObj);
+    UpdateBalances(minerStatsObj, userObj, poolStatsObj);
+    UpdateExchangeRates(poolStatsObj);
+    UpdateMinerData(minerStatsAllWorkersObj)
+    UpdateBlockData()
 }
 
 function UpdateStatusBar(allWorkers)
@@ -564,45 +540,32 @@ function UpdateStatusBar(allWorkers)
     //return prematurely in the event of an error occuring
     if (!allWorkers)
     {
-        //Set the API connection status
-        topApiStatusDisplay.innerHTML = "NETWORK ERROR"
-        topApiStatusDisplay.style.color = "red"
-        topApiStatusBlinkerDisplay.style.display = "none"
-
-        //Set the miner status to unknown if API cannot be reached
-        topStatusDisplay.innerHTML = "UNKNOWN";
+        topStatusDisplay.innerHTML = "ERROR";
         topStatusDisplay.style.color = "red";
-        statusBlinkerDisplay.style.display = "none"
         return;
     }
-
-    topApiStatusDisplay.style.color = "lightgreen"
-    topApiStatusDisplay.innerHTML = "CONNECTION OK"
-    topApiStatusBlinkerDisplay.style.display = "block"
-
+    
     let workerCount = Object.keys(allWorkers).length - 1;
 
     if (workerCount < 1)
     {
         topStatusDisplay.style.color = "yellow"
         topStatusDisplay.innerHTML = "NOT MINING";
-        statusBlinkerDisplay.style.display = "none"
     }
     else
     {
         topStatusDisplay.style.color = "lightgreen";
         topStatusDisplay.innerHTML = "MINERS ONLINE";
-        statusBlinkerDisplay.style.display = "block"
     }
 }
 
-function UpdateTopStats(netObj, poolObj)
-{   
+function UpdateTopStats(netObj, poolObj, worldApiObj)
+{
     poolHashrateDisplay.innerHTML = ParseHashrate(poolObj.pool_statistics.portHash[18081]) + "&nbsp&nbsp&nbsp&nbsp/&nbsp&nbsp&nbsp&nbsp" + ParseHashrate(poolObj.pool_statistics.hashRate);
     poolBlocksFoundDisplay.innerHTML = poolObj.pool_statistics.totalAltBlocksFound;
     poolXMRBlocksFoundDisplay.innerHTML = poolObj.pool_statistics.totalBlocksFound;
     blockchainHeightDisplay.innerHTML = netObj.main_height;
-    networkHashrateDisplay.innerHTML = ParseHashrate(netObj.difficulty / COINS[18081].time);
+    networkHashrateDisplay.innerHTML = ParseHashrate(worldApiObj.hashrate);
 }
 
 function UpdateMinerHashrates(obj)
@@ -818,26 +781,6 @@ function UnixTSToDate(unix_timestamp)
     const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
     let date = new Date(unix_timestamp).toLocaleTimeString("en-us", options)
     return date;
-}
-
-function InitiateBlinker()
-{
-    let interval = 1000
-    let char = '🟢'
-
-    setInterval( () =>
-    {
-        if (statusBlinkerDisplay.innerHTML == char)
-            statusBlinkerDisplay.innerHTML = "";
-        else
-            statusBlinkerDisplay.innerHTML = char;
-
-        if (topApiStatusBlinkerDisplay.innerHTML == char)
-            topApiStatusBlinkerDisplay.innerHTML = "";
-        else
-            topApiStatusBlinkerDisplay.innerHTML = char;
-
-    }, interval )
 }
 
 function ChangeTheme()
